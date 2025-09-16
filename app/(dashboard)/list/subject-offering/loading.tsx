860c143aabf7a4854dbaf98bc2ef95ac683d130c
DaniloBorreros <daniloborrerosjr@gmail.com>
2025-09-16 13:12:28 +0800

    (feat): skeleton

--- app/(dashboard)/list/subject-offering/loading.tsx
+++ app/(dashboard)/list/subject-offering/loading.tsx
@@ -1,7 +1,6 @@
 "use client";
 import SeedingSubjectOfferingSkeleton from "@/components/skeleton/SeedSubjectOfferingSkeleton";
 import React from "react";
-import { HashLoader } from "react-spinners";
 
 export default function loading() {
   return <SeedingSubjectOfferingSkeleton />;
