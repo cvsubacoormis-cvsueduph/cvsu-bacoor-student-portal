502624a401a158025c8d9dae3cd2168940974465
DaniloBorreros <daniloborrerosjr@gmail.com>
2026-01-20 00:48:10 +0800

    feat: Initialize Next.js root layout with custom fonts, metadata, ClerkProvider, and analytics script.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
 
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -28,6 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Script
+        async
+        src="https://analytics.cvsu-bacoor.com/script.js"
+        data-website-id="8316ee4b-feeb-4942-ac08-ebb67c27baea"
+      />
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
